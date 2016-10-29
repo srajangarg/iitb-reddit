3110b2ff4615a2029f1ca19b7b5351b58ecc1b7d
Rishabh <rishabhagarwal467@gmail.com>
2016-10-30 03:08:46 +0530

    added a script for generating test data with hardcoded values

---
+++ reddit/script.sh
@@ -0,0 +1,6 @@
+python2.7 manage.py loadtestdata subreddits.Subreddit:20
+python2.7 manage.py loadtestdata users.Redditer:10
+python2.7 manage.py loadtestdata users.Moderator:3
+python2.7 manage.py loadtestdata posts.TextPost:20
+python2.7 manage.py loadtestdata posts.LinkPost:25
+python2.7 manage.py loadtestdata posts.Comment:100
