a14c288d00856be111485a182d4589f014f1dda1
shrey <shrey_rajesh@hotamil.com>
2016-11-03 01:53:18 +0530

    links for text and link posts

--- reddit/users/views.py
+++ reddit/users/views.py
@@ -55,13 +55,13 @@ def user(request, username):
 
 def feed():
     posts = []
-    for p in LinkPost.objects.extra(select={'num_votes' : 0, 'num_comments' : 0}):
+    for p in LinkPost.objects.extra(select={'num_votes' : 0, 'num_comments' : 0, 'type' : 'link'}):
         p.num_votes = Vote.objects.filter(voted_on = p).aggregate(votes = Sum('value')).get('votes')
         p.num_comments = Comment.objects.filter(commented_on = p).count()
         if p.num_votes == None:
             p.num_votes = 0
         posts.append(p)
-    for p in TextPost.objects.extra(select={'num_votes' : 0, 'num_comments' : 0}):
+    for p in TextPost.objects.extra(select={'num_votes' : 0, 'num_comments' : 0, 'type' : 'text'}):
         p.num_votes = Vote.objects.filter(voted_on = p).aggregate(votes=Sum('value')).get('votes')
         p.num_comments = Comment.objects.filter(commented_on = p).count()
         if p.num_votes == None:
