2d6ee757024e4ab008ecbc94da2fabe5eeecde30
Rishabh <rishabhagarwal467@gmail.com>
2016-11-20 22:45:30 +0530

    Added the algorithms for top, new, hot for posts (will do rising & comment_ranking later)

--- reddit/users/urls.py
+++ reddit/users/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+	url(r'^$',views.index, name='index'),
+	url(r'^feed/top/(?P<sort_type>[a-z]*)$', views.top, name="feed"),
+    url(r'^feed/(.*)$', views.index, name='feed'),
     url(r'^login$', views.login, name='login'),
     url(r'^signup$', views.signup, name='signup'),
     url(r'^logout$', views.logout, name='logout'),
--- reddit/users/views.py
+++ reddit/users/views.py
@@ -1,17 +1,52 @@
 from django.http import HttpResponse
 from django.contrib.auth import get_user_model, authenticate, login as auth_login, logout as auth_logout
 from django.shortcuts import render, redirect
+from django.utils import timezone
 from subreddits.models import Subreddit
 from posts.models import TextPost, LinkPost, Comment, Vote
 from posts.views import updatePostFeatures
-import ldap
 
-def index(request):
+import ldap
+import calendar
+from datetime import datetime, timedelta
+from math import log
+
+# make this date timezone aware
+epoch = timezone.make_aware(datetime(1970, 1, 1), timezone.get_current_timezone())
+
+def epoch_seconds(date):
+    td = date - epoch
+    return td.days * 86400 + td.seconds + (float(td.microseconds) / 1000000)
+
+# To calculate the score of a post for the hot ranking algorithm
+def hot(num_votes, date):
+    order = log(max(abs(num_votes), 1), 10)
+    sign = 1 if num_votes > 0 else -1 if num_votes < 0 else 0
+    seconds = epoch_seconds(date) - 1134028003
+    return round(  (order * sign) + (seconds / 45000),   7)
+
+
+def index(request, ranking = ""):
+    # print ranking
     if request.user.is_authenticated():
-        posts = feed(request.user)
+        posts = feed(ranking,request.user)
     else:
-        posts = feed()
+        posts = feed(ranking)
     return render(request, "index.html", {"posts" : posts})
+
+top_sort_orders = ['','hour','day','month','year','all']
+
+def top(request, sort_type):
+    if(sort_type in top_sort_orders):
+        if(request.user.is_authenticated):
+            posts = top_feed(sort_type,request.user)
+        else:
+            posts = top_feed(sort_type)
+
+        return render(request,"index.html",{"posts" : posts})
+    else:
+        # TODO : redirect to some page saying lost!
+        print("lost")
 
 def login(request):
 
@@ -40,7 +75,7 @@ def ldap_auth(email, password):
     try:
         if search_result:
             authenticate = conn.bind_s(search_result[0][0], password)
-            return True            
+            return True
         else:
             return False
     except:
@@ -90,7 +125,7 @@ def userDownvoted(request, username):
     return render(request, "user.html", {"posts" : userposts, "username" : username})
 
 # def myaccount(request):
-# 
+#
 #     if request.user.is_authenticated():
 #         user_email = request.user.email
 #         return redirect('user/' + user_email)
@@ -101,30 +136,66 @@ def votedByUser(post, user, vote):
     qs = Vote.objects.filter(voted_on__id = post.id, voted_by__email = user)
     return len(qs) > 0 and int(qs[0].value) == vote
 
-def feed(user = None):
+def feed(ranking="", user = None):
 
     posts = []
 
-    for p in LinkPost.objects.extra(select={'num_votes' : 0, 'num_comments' : 0, 'type' : '%s', 'vote' : 0}, 
+    for p in LinkPost.objects.extra(select={'num_votes' : 0, 'num_comments' : 0, 'type' : '%s', 'vote' : 0},
                                     select_params=('link',)):
         posts.append(updatePostFeatures(p, user))
 
-    for p in TextPost.objects.extra(select = {'num_votes' : 0, 'num_comments' : 0, 'type' : '%s', 'vote' : 0}, 
+    for p in TextPost.objects.extra(select = {'num_votes' : 0, 'num_comments' : 0, 'type' : '%s', 'vote' : 0},
+                                    select_params = ('text',)):
+        posts.append(updatePostFeatures(p, user))
+    if(ranking == "" or ranking == "hot"):
+        return sorted(posts, key = lambda post : hot(post.num_votes, post.created_on),reverse=True)
+    elif(ranking == "new"):
+        return sorted(posts, key = lambda p: p.created_on, reverse=True)
+
+def monthdelta(date, delta):
+    m, y = (date.month+delta) % 12, date.year + ((date.month)+delta-1) // 12
+    if not m: m = 12
+    d = min(date.day, calendar.monthrange(y, m)[1])
+    return date.replace(day=d,month=m, year=y)
+
+def top_feed(sort_type,user = None):
+
+    print(sort_type)
+    time_to_compare = datetime.now() #datetime.now()
+    time_to_compare = timezone.make_aware(time_to_compare, timezone.get_current_timezone())
+    if(sort_type == 'hour'):
+        time_to_compare -= timedelta(hours = 1)
+    elif(sort_type == 'day'):
+        time_to_compare -= timedelta(days = 1)
+    elif(sort_type == 'month'):
+        time_to_compare = monthdelta(time_to_compare, -1)
+    elif(sort_type == 'year'):
+        time_to_compare = monthdelta(time_to_compare, -12)
+    elif(sort_type == 'all' or sort_type == ""):
+        time_to_compare = epoch
+
+    posts = []
+
+    for p in LinkPost.objects.filter(created_on__gte=time_to_compare).\
+        extra(select={'num_votes' : 0, 'num_comments' : 0, 'type' : '%s', 'vote' : 0},
+                                    select_params=('link',)):
+        posts.append(updatePostFeatures(p, user))
+
+    for p in TextPost.objects.filter(created_on__gte=time_to_compare).extra(select = {'num_votes' : 0, 'num_comments' : 0, 'type' : '%s', 'vote' : 0},
                                     select_params = ('text',)):
         posts.append(updatePostFeatures(p, user))
 
     return sorted(posts, key = lambda p: p.num_votes, reverse=True)
 
-
 def userPosts(username, user = None):
 
     posts = []
 
-    for p in LinkPost.objects.filter(posted_by__email = username).extra(select={'num_votes' : 0, 'num_comments' : 0, 'type' : '%s', 'vote' : 0}, 
+    for p in LinkPost.objects.filter(posted_by__email = username).extra(select={'num_votes' : 0, 'num_comments' : 0, 'type' : '%s', 'vote' : 0},
                                     select_params=('link',)):
         posts.append(updatePostFeatures(p, user))
 
-    for p in TextPost.objects.filter(posted_by__email = username).extra(select = {'num_votes' : 0, 'num_comments' : 0, 'type' : '%s', 'vote' : 0}, 
+    for p in TextPost.objects.filter(posted_by__email = username).extra(select = {'num_votes' : 0, 'num_comments' : 0, 'type' : '%s', 'vote' : 0},
                                     select_params = ('text',)):
         posts.append(updatePostFeatures(p, user))
 
@@ -132,7 +203,7 @@ def userPosts(username, user = None):
 
 def userVotedPosts(username, vote, user = None):
 
-    posts = feed(user)
+    posts = feed("",user)
     posts = [p for p in posts if votedByUser(p, username, vote)]
 
     return sorted(posts, key = lambda p: p.created_on, reverse=True)
