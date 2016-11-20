38a199a44a644f188ef11dad495d7602ad94f2ff
anuj <anujmittal0801@gmail.com>
2016-11-20 15:39:16 +0530

    SUbreddit page backend and submit comment

--- reddit/posts/urls.py
+++ reddit/posts/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^submitpost$', views.submitpost, name='submitpost'),
-    url(r'^submit$', views.newpost, name='newpost'),
+    url(r'^submitpost$', views.submitPost, name='submitpost'),
+    url(r'^submitcomment$', views.submitComment, name='submitcomment'),
+    url(r'^submit$', views.newPost, name='newpost'),
     url(r'^vote$', views.vote, name='vote'),
     url(r'^([0-9]*)$', views.post, name='post'),
 ]
--- reddit/subreddits/views.py
+++ reddit/subreddits/views.py
@@ -1,5 +1,33 @@
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, redirect
+from .models import *
+from posts.models import TextPost, LinkPost, Comment, Vote
+from posts.views import updatePostFeatures
+
+def index(request, subreddit_title):
+    try:
+        subreddit = Subreddit.objects.get(title=subreddit_title)
+    except:
+        return HttpResponse("Subreddit Does Not Exist")
+
+    if request.user.is_authenticated():
+        posts = feed(subreddit, request.user)
+    else:
+        posts = feed(subreddit)
+    return render(request, "index.html", {"subreddit" : subreddit, "posts" : posts})
 
 
-def index(request):
-    return HttpResponse("Hello, you're at the subreddits page!")
+def feed(subreddit, user = None):
+
+    posts = []
+
+    for p in LinkPost.objects.filter(posted_in = subreddit).extra(select={'num_votes' : 0, 'num_comments' : 0, 'type' : '%s', 'vote' : 0}, 
+                                    select_params=('link',)):
+        posts.append(updatePostFeatures(p, user))
+
+    for p in TextPost.objects.filter(posted_in = subreddit).extra(select = {'num_votes' : 0, 'num_comments' : 0, 'type' : '%s', 'vote' : 0}, 
+                                    select_params = ('text',)):
+        posts.append(updatePostFeatures(p, user))
+
+    return sorted(posts, key = lambda p: p.num_votes, reverse=True)
